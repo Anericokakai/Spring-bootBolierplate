c081bdb1b644e30d049d6ad32d8da115eddb1015
Anericokakai <challykiddoh@gmail.com>
2024-06-24 11:52:19 +0300

    fix some

---
+++ src/main/java/com/d3f4ult/boilerplate/BoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.d3f4ult.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilerPlateApplication.class, args);
+    }
+
+}
